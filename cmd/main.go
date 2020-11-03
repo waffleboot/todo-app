202fb9964407e85dad91c426056b76a63176cfec
Andrei Yangabishev <waffleboot@gmail.com>
2020-11-03 05:19:38 +0300

    #4

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/spf13/viper"
 	"github.com/waffleboot/todo-app"
 	"github.com/waffleboot/todo-app/pkg/handler"
 	"github.com/waffleboot/todo-app/pkg/repository"
@@ -10,11 +11,20 @@ import (
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
